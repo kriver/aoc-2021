ae489779feaa1568544f42b4182dd873ba4e16f9
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-01 07:40:21 +0100

    day 1

---
+++ day1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from util import *
+
+
+def count_increases(data: List[int], window : int) -> int:
+    cnt = 0
+    prev = sum(data[0:window])
+    for i in range(window, len(data)):
+        new = prev + data[i] - data[i - window]
+        cnt += 1 if new > prev else 0
+        prev = new
+    return cnt
+
+
+if __name__ == "__main__":
+    measures = as_int(load('day1.txt'))
+
+    increases = count_increases(measures, 1)
+    assert increases == 1722
+    print("Part 1: 0-window increases = %d" % increases)
+
+    increases = count_increases(measures, 3)
+    assert increases == 1748
+    print("Part 1: 3-window increases = %d" % increases)
