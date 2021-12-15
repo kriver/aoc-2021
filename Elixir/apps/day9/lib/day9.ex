df5bd7b37ea0879ab23307f7e1f05684228125f6
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-15 18:22:20 +0100

    Elixir day 9 (part 2)

--- Elixir/apps/day9/lib/day9.ex
+++ Elixir/apps/day9/lib/day9.ex
@@ -43,15 +43,72 @@ defmodule Day9 do
     )
   end
 
-  def low_point_risks() do
+  def prepare() do
     height_map = parse_input()
                  |> add_neighbour_heights()
                  |> Map.map(fn {_k, {h, o}} -> {h, length(o)} end)
     low_points = height_map
                  |> Map.filter(fn {_key, {_height, nb}} -> nb == 4 end)
+    {height_map, low_points}
+  end
+
+  def low_point_risks() do
+    {_, low_points} = prepare()
     low_points
     |> Map.values()
     |> Stream.map(fn {height, _} -> height + 1 end)
     |> Enum.sum()
+  end
+
+  def valid_neighbours_not_visited(map, {x, y}, visited) do
+    @neighbours
+    |> Stream.map(fn {dx, dy} -> {x + dx, y + dy} end)
+    |> Stream.filter(fn pos -> Map.has_key?(map, pos) end)
+    |> Stream.filter(fn pos -> !MapSet.member?(visited, pos) end)
+    |> Stream.map(fn pos -> {h, _} = Map.get(map, pos, {9, []});{pos, h} end)
+    |> Stream.filter(fn {_pos, h} -> h != 9 end)
+    |> Stream.map(fn {pos, _h} -> pos end)
+    |> Enum.to_list()
+  end
+
+  def visit_one(map, pos, visited) do
+    new_visited = MapSet.put(visited, pos)
+    grow(
+      map,
+      valid_neighbours_not_visited(map, pos, new_visited),
+      new_visited
+    )
+  end
+
+  def grow(_map, [], visited) do
+    visited
+  end
+
+  def grow(map, queue, visited) do
+    queue
+    |> Enum.reduce(
+         visited,
+         fn pos, acc -> visit_one(map, pos, acc) end
+       )
+  end
+
+  def build_basins() do
+    {height_map, low_points} = prepare()
+    low_points
+    |> Map.keys()
+    |> Stream.map(fn pos -> grow(height_map, [pos], MapSet.new()) end)
+  end
+
+  def basin_sizes() do
+    build_basins()
+    |> Stream.map(&Enum.count/1)
+    |> Enum.to_list()
+  end
+
+  def product_largest_basin_sizes(num) do
+    basin_sizes()
+    |> Enum.sort(&(&1 > &2))
+    |> Enum.slice(0..(num - 1))
+    |> Enum.reduce(1, fn b, acc -> acc * b end)
   end
 end
--- Elixir/apps/day9/test/day9_test.exs
+++ Elixir/apps/day9/test/day9_test.exs
@@ -5,4 +5,8 @@ defmodule Day9Test do
   test "Low points" do
     assert Day9.low_point_risks() == 480
   end
+
+  test "Largest basins" do
+    assert Day9.product_largest_basin_sizes(3) == 1045660
+  end
 end
