0e2cb2287a32c30f3755eff14fc9a00382788540
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-19 13:34:16 +0100

    Python day 19 (part 1)

--- Python/day19.py
+++ Python/day19.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
 import itertools
 from collections import Counter
-from functools import reduce
-from typing import Tuple, List, Set, Dict
+from typing import Tuple, List, Set
+
+import numpy as np
 
 from util import load
 
 Coord = Tuple[int, int, int]
 
+ROTATIONS = [np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]]),
+             np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]]),
+             np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]]),
+             np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+             np.array([[-1, 0, 0], [0, 0, -1], [-0, -1, -0]]),
+             np.array([[-1, 0, 0], [0, 0, 1], [0, 1, -0]]),
+             np.array([[1, 0, 0], [0, 0, -1], [-0, 1, 0]]),
+             np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]]),
+             np.array([[0, -1, 0], [-1, 0, 0], [-0, -0, -1]]),
+             np.array([[0, -1, 0], [1, 0, 0], [-0, 0, 1]]),
+             np.array([[0, 1, 0], [-1, 0, 0], [0, -0, 1]]),
+             np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]]),
+             np.array([[0, -1, 0], [0, 0, -1], [1, 0, 0]]),
+             np.array([[0, -1, 0], [0, 0, 1], [-1, 0, 0]]),
+             np.array([[0, 1, 0], [0, 0, -1], [-1, 0, 0]]),
+             np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]]),
+             np.array([[0, 0, -1], [-1, 0, 0], [0, 1, 0]]),
+             np.array([[0, 0, -1], [1, 0, 0], [0, -1, 0]]),
+             np.array([[0, 0, 1], [-1, 0, 0], [0, -1, 0]]),
+             np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]]),
+             np.array([[0, 0, -1], [0, -1, 0], [-1, -0, -0]]),
+             np.array([[0, 0, -1], [0, 1, 0], [1, -0, 0]]),
+             np.array([[0, 0, 1], [0, -1, 0], [1, 0, -0]]),
+             np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])]
+
 
 class Scanner:
     def __init__(self, scanner_id: int, scans: List[Coord]):
         self._id: int = scanner_id
-        self._beacons: List[Coord] = scans
-        self._counts: Dict[Coord, int] = {c: 1 for c in self._beacons}
-        self._dist_list: List[int] = [self._dist(p1, p2) for p1, p2 in itertools.combinations(scans, 2)]
-        self._dist_set: Set[int] = set(self._dist_list)
-        # assert len(self._dist_list) == len(self._dist_set)
-        if len(self._dist_list) != len(self._dist_set):
-            print(f'Problem with scanner {self._id}')
+        self._beacons: Set[Coord] = set(scans)
+        self._dist_set: Set[int] = self._distances()
 
     def __repr__(self):
         return f'Scanner[{self._id}/{len(self._beacons)}]'
@@ -29,37 +50,45 @@ class Scanner:
         x2, y2, z2 = p2
         return (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
 
-    def _to_beacons(self, dist):
-        idx = self._dist_list.index(dist)
-        n = len(self._beacons) - 1
-        first = 0
-        while idx >= n:
-            first += 1
-            idx -= n
-            n -= 1
-        p1 = self._beacons[first]
-        p2 = self._beacons[first + idx + 1]
-        # assert self._dist(p1, p2) == dist
-        return {p1, p2}
-
-    def _increment_all(self, overlap: Set[int]):
-        beacons = reduce(lambda acc, dist: acc | self._to_beacons(dist), overlap, set())
-        for b in beacons:
-            self._counts[b] += 1
+    @staticmethod
+    def _dist_tuple(p1: Coord, p2: Coord) -> Coord:
+        x1, y1, z1 = p1
+        x2, y2, z2 = p2
+        return (x1 - x2), (y1 - y2), (z1 - z2)
 
     def num_beacons(self):
         return len(self._beacons)
 
-    def beacon_counts(self):
-        return self._counts.values()
+    def overlaps(self, other: 'Scanner'):
+        return len(self._dist_set & other._dist_set) >= 12
 
-    def calc_overlaps(self, other: 'Scanner'):
-        print(f'Checking {self} and {other}')
-        overlap = self._dist_set & other._dist_set
-        if len(overlap) >= 66:  # 12 points -> 66 distances (11+10+9+...+1)
-            print(f'Overlap of {len(overlap)} distances for {self} and {other}')
-            self._increment_all(overlap)
-            other._increment_all(overlap)
+    def combine(self, other: 'Scanner'):
+        rotations = other._all_rotations()
+        for rotation in rotations:
+            distances = [self._dist_tuple(beacon, other_beacon)
+                         for other_beacon in rotation
+                         for beacon in self._beacons]
+            counted = Counter(distances)
+            for distance, count in counted.items():
+                if count >= 12:
+                    self._add(rotation, distance)
+                    return True
+        return False
+
+    def _distances(self):
+        return set([self._dist(p1, p2) for p1, p2 in itertools.combinations(self._beacons, 2)])
+
+    def _all_rotations(self) -> List[List[Coord]]:
+        return [[tuple(np.dot(rotation, np.array(beacon)))
+                 for beacon in self._beacons]
+                for rotation in ROTATIONS]
+
+    def _add(self, other: List[Coord], dist: Coord):
+        dx, dy, dz = dist
+        moved = {(x + dx, y + dy, z + dz) for x, y, z in other}
+        self._beacons |= moved
+        # FIXME optimize
+        self._dist_set = self._distances()
 
 
 def parse(lines: List[str]) -> List[Scanner]:
@@ -82,16 +111,22 @@ def parse(lines: List[str]) -> List[Scanner]:
 if __name__ == "__main__":
     scanners = parse(load('day19.txt'))
 
-    total_scanned_beacons = sum([s.num_beacons() for s in scanners])
-    for scanner1, scanner2 in itertools.combinations(scanners, 2):
-        scanner1.calc_overlaps(scanner2)
+    solution = scanners[0]
+    solved = [scanners[0]]
+    unsolved = scanners[1:]
+    while unsolved:
+        before = len(unsolved)
+        for u in unsolved:
+            for s in solved:
+                if s.overlaps(u):
+                    if solution.combine(u):
+                        solved.append(u)
+                        unsolved.remove(u)
+                        print(f'{solution}')
+                        break
+        assert before != len(unsolved)
+    print(f'{solution}')
 
-    all_counts = reduce(lambda acc, l: acc + l, [list(s.beacon_counts()) for s in scanners])
-    grouped_counts = Counter(all_counts)
-    assert sum(grouped_counts.values()) == total_scanned_beacons
-    unique_counts = [v / k for k, v in grouped_counts.items()]
-    unique = sum(unique_counts)
-
-    print(f'Total beacons = {total_scanned_beacons}')
+    unique = solution.num_beacons()
     print(f'Unique beacons = {unique}')
     assert unique == 403
