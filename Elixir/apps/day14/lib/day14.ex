30ebed3ebdd1a390ada890715db324a466675770
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-19 15:33:15 +0100

    Elixir day 14

--- Elixir/apps/day14/lib/day14.ex
+++ Elixir/apps/day14/lib/day14.ex
@@ -10,27 +10,59 @@ defmodule Day14 do
                        |> Stream.map(fn s -> String.split(s, " -> ") end)
                        |> Stream.map(&List.to_tuple/1)
                        |> Enum.to_list()
-    {String.split(start, "", trim: true), Enum.into(tail, %{})}
+    template = String.split(start, "", trim: true)
+    element_counts = template
+                     |> Enum.frequencies()
+    pair_counts = template
+                  |> Enum.chunk_every(2, 1, :discard)
+                  |> Enum.map(&List.to_tuple/1)
+                  |> Enum.frequencies()
+    rules = Enum.reduce(
+      tail,
+      %{},
+      fn {from, to}, acc ->
+        Map.put(
+          acc,
+          String.split(from, "", trim: true)
+          |> List.to_tuple(),
+          to
+        )
+      end
+    )
+    {element_counts, pair_counts, rules}
   end
 
-  def evolve([a], _rules), do: [a]
-  def evolve([a | [b | template]], rules) do
-    [a, rules[a <> b]] ++ evolve([b | template], rules)
+  def evolve(ec, pc, rules) do
+    pc
+    |> Map.to_list()
+    |> Enum.reduce(
+         {ec, %{}},
+         fn {{a, c}, cnt}, {nec, npc} ->
+           b = rules[{a, c}]
+           {
+             nec
+             |> Map.update(b, cnt, &(&1 + cnt)),
+             npc
+             |> Map.update({a, b}, cnt, &(&1 + cnt))
+             |> Map.update({b, c}, cnt, &(&1 + cnt))
+           }
+         end
+       )
   end
 
-  def evolve(template, _rules, 0), do: template
-  def evolve(template, rules, generations) do
-    evolve(evolve(template, rules), rules, generations - 1)
+  def evolve(ec, _pc, _rules, 0), do: ec
+  def evolve(ec, pc, rules, generations) do
+    {nec, npc} = evolve(ec, pc, rules)
+    evolve(nec, npc, rules, generations - 1)
   end
 
   def evolve(generations) do
-    {template, rules} = input()
-    evolve(template, rules, generations)
+    {ec, pc, rules} = input()
+    evolve(ec, pc, rules, generations)
   end
 
-  def score(template) do
-    {minimum, maximum} = template
-                         |> Enum.frequencies()
+  def score(ec) do
+    {minimum, maximum} = ec
                          |> Map.to_list()
                          |> Enum.reduce(
                               fn {_elem, count}, {minimum, maximum} ->
