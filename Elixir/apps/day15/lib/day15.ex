4ae954b2107ad301d396a2e4f474726055018784
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-19 19:14:36 +0100

    Elixir day 15 (part 1)

---
+++ Elixir/apps/day15/lib/day15.ex
@@ -0,0 +1,65 @@
+defmodule Day15 do
+  @moduledoc """
+  Documentation for `Day15`.
+  """
+
+  @delta [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
+
+  def input() do
+    File.read!("data/day15.txt")
+    |> String.split("\n")
+    |> Stream.filter(&(String.length(&1) > 0))
+    |> Stream.with_index()
+    |> Enum.reduce(
+         %{},
+         fn {line, y}, acc ->
+           String.split(line, "", trim: true)
+           |> Stream.with_index()
+           |> Enum.reduce(
+                acc,
+                fn {risk, x}, acc ->
+                  Map.put(acc, {x, y}, String.to_integer(risk))
+                end
+              )
+         end
+       )
+  end
+
+  def add_sorted([], new_rp), do: [new_rp]
+  def add_sorted([{risk, pos} | queue], {new_risk, new_pos}) when new_risk <= risk do
+    [{new_risk, new_pos}, {risk, pos}] ++ queue
+  end
+  def add_sorted([rp | queue], new_rp), do: [rp | add_sorted(queue, new_rp)]
+
+  def find_path([{total_risk, current} | _q], _v, _mr, _r, dest) when current == dest, do: total_risk
+  def find_path([{total_risk, {x, y}} | queue], visited, max_risks, risks, dest) do
+    {nq, nv, nmr} = @delta
+                    |> Enum.reduce(
+                         {queue, visited, max_risks},
+                         fn {dx, dy}, {q, v, mr} ->
+                           new_pos = {x + dx, y + dy}
+                           if Map.has_key?(risks, new_pos) and !MapSet.member?(v, new_pos) do
+                             new_risk = total_risk + risks[new_pos]
+                             {
+                               add_sorted(q, {new_risk, new_pos}),
+                               MapSet.put(v, new_pos),
+                               Map.update(mr, new_pos, new_risk, &(min(&1, new_risk)))
+                             }
+                           else
+                             {q, v, mr}
+                           end
+                         end
+                       )
+    find_path(nq, nv, nmr, risks, dest)
+  end
+  def find_path(risks, start, dest) do
+    visited = MapSet.new([start])
+    queue = [{0, start}]
+    find_path(queue, visited, %{}, risks, dest)
+  end
+
+  def find_path() do
+    input()
+    |> find_path({0, 0}, {99, 99})
+  end
+end
