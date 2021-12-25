b438539cb344c26427d0ce8c68e55426bacd6dea
Kris Verbeeck <kris_verbeeck@fastmail.fm>
2021-12-25 07:20:02 +0100

    Python day 23 (part 2)

--- Python/day23.py
+++ Python/day23.py
@@ -22,96 +22,122 @@ def amphipod(floor, pos):
     return floor[y][x]
 
 
-def needs_to_move(floor, amph: str, pos):
+def needs_to_move(floor, levels, amph: str, pos):
     x, y = pos
     if AMPHIPOD_ROOMS[amph[0]] == x:
         # either we're at back of the room, or we're at the front and
-        # our friend is already at the back
-        if y == 2 or floor[2][x][0] == amph[0]:
+        # our friends are already at the back
+        if not [ry for ry in range(y + 1, levels + 1)
+                if floor[ry][x][0] != amph[0]]:
             return False
     return True
 
 
-def possible_moves(floor, amph: str, pos):
+def possible_moves(floor, levels, amph: str, pos):
     x, y = pos
     moves = []
     if y == 0:
         # can we go to our room?
         rx = AMPHIPOD_ROOMS[amph[0]]
         ry = 0
-        # room empty?
-        if floor[2][rx] == '.':
-            ry = 2
-        # room already contains our friend at the back?
-        elif floor[2][rx][0] == amph[0] and floor[1][rx] == '.':
-            ry = 1
-        if ry:
+        # room already contains our friends at the back?
+        for ry in range(levels, 0, -1):
+            if floor[ry][rx][0] != amph[0]:
+                break
+        if floor[ry][rx][0] == '.':
             # hall free towards room?
             if not [p for p in range(min(x, rx), max(x, rx) + 1)
                     if floor[0][p] != '.']:
                 moves.append((rx, ry))
-    elif y == 1 or floor[1][x] == '.':
-        # move out of room
-        for p in range(x - 1, -1, -1):
-            if p in HALL_POSITIONS:
-                if floor[0][p] == '.':
-                    moves.append((p, 0))
+    # move out of room
+    elif not [ry for ry in range(y - 1, 0, -1) if floor[ry][x] != '.']:
+        for rx in range(x - 1, -1, -1):
+            if rx in HALL_POSITIONS:
+                if floor[0][rx] == '.':
+                    moves.append((rx, 0))
                 else:
                     break
-        for p in range(x + 1, 11):
-            if p in HALL_POSITIONS:
-                if floor[0][p] == '.':
-                    moves.append((p, 0))
+        for rx in range(x + 1, 11):
+            if rx in HALL_POSITIONS:
+                if floor[0][rx] == '.':
+                    moves.append((rx, 0))
                 else:
                     break
     return moves
 
 
-def is_solved(floor: List[List[str]]):
-    return ''.join([p[0] for p in floor[1] if p[0] in 'ABCD']) == 'ABCD' \
-           and ''.join([p[0] for p in floor[2] if p[0] in 'ABCD']) == 'ABCD'
+def is_solved(floor: List[List[str]], levels):
+    solved = True
+    for i in range(levels):
+        solved &= ''.join([p[0]
+                           for p in floor[i + 1]
+                           if p[0] in 'ABCD']) == 'ABCD'
+    return solved
 
 
-def solve(floor, positions, energy, minimum, dejavu):
+def solve(floor, levels, positions, energy, minimum, dejavu):
     # print(f'Dejavu = {len(dejavu)}')
-    if is_solved(floor):
-        print(f'Found {energy} - minimum before {minimum}')
+    if is_solved(floor, levels):
+        # print(f'Found {energy} - minimum before {minimum}')
         return min(minimum, energy)
     for pos in positions:
         x, y = pos
         amph = amphipod(floor, pos)
-        if needs_to_move(floor, amph, pos):
+        if needs_to_move(floor, levels, amph, pos):
             floor[y][x] = '.'  # move out
-            moves = possible_moves(floor, amph, pos)
+            moves = possible_moves(floor, levels, amph, pos)
             for npos in moves:
                 nx, ny = npos
                 floor[ny][nx] = amph
                 new_energy = energy + needed_energy(pos, npos, amph)
-                key = ''.join([''.join(l) for l in floor])
+                key = ''.join([''.join(p) for p in floor])
                 if key not in dejavu or dejavu[key] > new_energy:
                     dejavu[key] = new_energy
                     new_positions = positions.copy()
                     new_positions.remove(pos)
                     new_positions.add(npos)
-                    e = solve(floor, new_positions, new_energy, minimum, dejavu)
+                    e = solve(floor, levels, new_positions, new_energy, minimum, dejavu)
                     minimum = min(e, minimum)
                 floor[ny][nx] = '.'
             floor[y][x] = amph  # and back in
     return minimum
 
 
-def main():
+def solve1():
     floor = [list('...........'),
+             # Test
              # ['.', '.', 'B1', '.', 'C1', '.', 'B2', '.', 'D2', '.', '.'],
              # ['.', '.', 'A1', '.', 'D1', '.', 'C2', '.', 'A2', '.', '.']
+             # Actual
              ['.', '.', 'A1', '.', 'D1', '.', 'B1', '.', 'D2', '.', '.'],
              ['.', '.', 'B2', '.', 'C1', '.', 'A2', '.', 'C2', '.', '.']
              ]
     positions = {(2 * x, y + 1) for x in range(1, 5) for y in range(2)}
-    energy = solve(floor, positions, 0, sys.maxsize, {})
+    energy = solve(floor, 2, positions, 0, sys.maxsize, {})
     print(f'Total energy used = {energy}')
     assert energy == 12240
 
 
+def solve2():
+    floor = [list('...........'),
+             # Test
+             # ['.', '.', 'B1', '.', 'C1', '.', 'B2', '.', 'D2', '.', '.'],
+             # ['.', '.', 'D3', '.', 'C3', '.', 'B3', '.', 'A4', '.', '.'],
+             # ['.', '.', 'D4', '.', 'B3', '.', 'A3', '.', 'C4', '.', '.'],
+             # ['.', '.', 'A1', '.', 'D1', '.', 'C2', '.', 'A2', '.', '.']
+             # Actual
+             ['.', '.', 'A1', '.', 'D1', '.', 'B1', '.', 'D2', '.', '.'],
+             ['.', '.', 'D3', '.', 'C3', '.', 'B3', '.', 'A4', '.', '.'],
+             ['.', '.', 'D4', '.', 'B3', '.', 'A3', '.', 'C4', '.', '.'],
+             ['.', '.', 'B2', '.', 'C1', '.', 'A2', '.', 'C2', '.', '.']
+             ]
+    positions = {(2 * x, y + 1) for x in range(1, 5) for y in range(4)}
+    energy = solve(floor, 4, positions, 0, sys.maxsize, {})
+    print(f'Total energy used = {energy}')
+    assert energy == 44618
+
+
 if __name__ == "__main__":
-    main()
+    solve1()
+
+    solve2()
